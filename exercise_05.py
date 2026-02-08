cc137c5ce02009fef604d5f584e1f5849561565c
BoraAbramov <borya812@gmail.com>
2026-02-08 21:29:03 +0200

    update

--- exercise_05.py
+++ exercise_05.py
@@ -19,3 +19,27 @@ for item in list1 + list2 + list3:
     list4.append(item)
 
 print(list4)
+
+#3
+list1 = [1, 2, 3, 4, 5]
+list2 = [2, 4]
+list3 = []
+
+for item in list1:
+    if item not in list2:
+        list3.append(item)
+
+print(list3)
+
+#4
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 6, 7]
+list3 = []
+for item in list1:
+    if item in list2:
+        list3.append(item)
+
+print(list3)
+
+#5
+
