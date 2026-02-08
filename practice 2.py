6539db9cca6c4c67b0fc1673a7626ea7bd1782fa
BoraAbramov <borya812@gmail.com>
2026-02-08 20:03:01 +0200

    prak 2 done

--- practice 2.py
+++ practice 2.py
@@ -10,13 +10,15 @@ while grade_list.__len__() < 10:
 
 _sum = 0
 for item in grade_list:
-    _sum = _sum + item
-print(_sum)
+    _sum += item
+print(f"sum {_sum}")
 
 _avg = _sum / grade_list.__len__()
-print(_avg)
+print(f"avarage {_avg}")
 
+print(f"the list {grade_list}")
 grade_list.pop(9)
+print(f"the list after pop 9 {grade_list}")
 
 _min_grade = 100
 _max_grade = 0
@@ -26,8 +28,11 @@ for item in grade_list:
     if item > _max_grade:
         _max_grade = item
 
+grade_list.remove(_min_grade)
 
+grade_list.insert(4, _max_grade)
+grade_list.append(_max_grade)
 
-print(_min_grade, _max_grade)
-print(grade_list)
+print(f"minimum grade {_min_grade}, maximum grade {_max_grade}")
+print(f"the list {grade_list}")
 
